0245eea728c6758aabe6cfc6213edc332bfa5537
Alexander Smirnov <alexandr.smirnov@corp.mail.ru>
2016-06-06 22:20:38 +0300

    Initial Commit

---
+++ funcobjc/funcobjc.h
@@ -0,0 +1,13 @@
+//
+//  funcobjc.h
+//  funcobjc
+//
+//  Created by Alexander Smirnov on 06/06/16.
+//  Copyright © 2016 Alexander Smirnov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface funcobjc : NSObject
+
+@end
