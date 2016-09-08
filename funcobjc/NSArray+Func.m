295d2ae9cbb330cbe8c078644c2b54541d5d3da2
Nikolay Morev <n.morev@corp.mail.ru>
2016-09-08 18:33:50 +0300

    Additional NSArray methods

--- funcobjc/NSArray+Func.m
+++ funcobjc/NSArray+Func.m
@@ -27,6 +27,19 @@ typedef id (^Function2)(id, id);
     };
 }
 
+- (NSArray *(^)(NSArray *(^)(id))) f_flattenMap {
+    return ^NSArray *(NSArray *(^f)(id)) {
+        NSMutableArray *array = self.f_reduce([NSMutableArray array], ^id(NSMutableArray* initial, id el) {
+            NSArray *result = f(el);
+            if (result.count > 0) {
+                [initial addObjectsFromArray:result];
+            }
+            return initial;
+        });
+        return [NSArray arrayWithArray:array];
+    };
+}
+
 - (NSArray * (^)(Predicate))f_filter {
     return ^NSArray * (Predicate f) {
         NSMutableArray *array = self.f_reduce([NSMutableArray arrayWithCapacity: self.count], ^NSMutableArray * (NSMutableArray *result, id el) {
@@ -49,6 +62,38 @@ typedef id (^Function2)(id, id);
     };
 }
 
+- (BOOL (^)(BOOL (^)(id)))f_all {
+    return ^BOOL (BOOL(^predicate)(id)) {
+        return !self.f_any(^BOOL(id obj) {
+            return !predicate(obj);
+        });
+    };
+}
+
+- (BOOL(^)(BOOL(^)(id)))f_any {
+    return ^BOOL (BOOL(^predicate)(id)) {
+        for (id item in self) {
+            if (predicate(item)) {
+                return YES;
+            }
+        }
+        return NO;
+    };
+}
+
+- (id (^)(BOOL(^)(id)))f_first {
+    return ^id(BOOL (^predicate)(id)) {
+        id passedObject = nil;
+        for (id object in self) {
+            if (predicate(object)) {
+                passedObject = object;
+                break;
+            }
+        }
+        return passedObject;
+    };
+}
+
 - (NSDictionary *)f_dict {
     return self.f_reduce(@{}.mutableCopy, ^id(NSMutableDictionary* initial, id el) {
         return (initial[el] = el, initial);
@@ -59,6 +104,18 @@ typedef id (^Function2)(id, id);
     return ^NSString* (NSString *j) {
         return [self componentsJoinedByString:j];
     };
+}
+
+- (NSArray *)f_flatten {
+    NSMutableArray *array = @[].mutableCopy;
+    [self enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        if ([obj isKindOfClass:NSArray.class]) {
+            [array addObjectsFromArray:(NSArray *)obj];
+        } else {
+            [array addObject:obj];
+        }
+    }];
+    return array.copy;
 }
 
 @end
